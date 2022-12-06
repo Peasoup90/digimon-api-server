72739437e1e5bb442147d55c3890d2724d305831
Peasoup90 <113778276+Peasoup90@users.noreply.github.com>
2022-12-06 19:02:40 +0000

    skeleton

--- server.js
+++ server.js
@@ -1,2 +1,35 @@
 'use strict';
 
+const express = require('express');
+const { get } = require('superagent');
+const app = express();
+const superagent = require('superagent');
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.status(200).send('home route');
+});
+
+app.get('/alldigimon/', (req, res) => {
+    const url = "https://digimon-api.vercel.app/api/digimon"
+    superagent
+    .get(url)
+    .then ((digiData) => {
+    const digimon = digiData.body.map((digimon) => digimon);
+        res.status(200).send(digimon);
+    })
+    .catch((error) => {
+        res.status(500).send(`something went wrong ${error}`);
+    });
+});
+
+
+
+app.get("*", (req, res) => {
+    res.status(404).send("not found");
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+});
